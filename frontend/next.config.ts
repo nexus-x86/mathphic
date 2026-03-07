049966f37eb92197d51c3801238d88f6afa0bd70
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2026-03-07 18:38:23 -0500

    good

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
-    process.env.NODE_ENV === "development"
+    return process.env.NODE_ENV === "development"
       ? [
         {
           source: "/api/:path*",
           destination: "http://127.0.0.1:8000/api/:path*",
         },
       ]
-      : [],
+      : [];
   },
 };
 
